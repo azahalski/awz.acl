7fd0427ba3fff2629496fd57cd1f93d6f8722f0c
Andrew Zahalski <andrew@zahalski.dev>
2025-04-26 15:11:03 +0300

    gulp checkpoint

---
+++ update/1.0.2/lang/ru/options.php
@@ -0,0 +1,7 @@
+<?php
+$MESS['AWZ_ACL_OPT_TITLE'] = "AWZ: Права доступа";
+$MESS['AWZ_ACL_OPT_L_BTN_SAVE'] = "Сохранить настройки";
+$MESS['AWZ_ACL_OPT_SECT2'] = "Права доступа";
+$MESS['AWZ_ACL_OPT_SECT1'] = "Общие настройки";
+$MESS['AWZ_ACL_OPT_SHOW_DESC'] = "Данный модуль предназначен для разработки и организации прав доступа по ролям - <a target=\"_blank\" href=\"https://zahalski.dev/modules/prava-dostupa-rolevaya-model/\">Документация</a>";
+
